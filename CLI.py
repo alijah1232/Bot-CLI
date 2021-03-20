de135065357182b539b8498666bc2509eb36683b
alijah1232 <67612208+alijah1232@users.noreply.github.com>
2021-03-19 19:34:24 -0700

    Add files via upload

---
+++ CLI.py
@@ -0,0 +1,51 @@
+import termcolor
+from colorama import Fore, init
+import termcolor
+import pyperclip
+import random
+import time
+import random
+import termcolor
+import pyperclip
+
+
+       
+        
+       
+        
+        
+            
+print(Fore.RED + """\
+
+
+                                                      
+ _____         _     _ _   _____ _____    ___     ___ 
+|   __|_ _ ___| |___|_| |_|     |     |  |_  |   |   |
+|   __|_'_| . | | . | |  _|-   -|  |  |   _| |_ _| | |
+|_____|_,_|  _|_|___|_|_| |_____|_____|  |_____|_|___|
+          |_|                                         
+
+""" + Fore.RESET)
+
+
+print(" Easy CLI ")
+
+print('''
+ChangeLogs:
+[] 
+[] 
+
+''')
+mode = int(input('''Enter:
+    (1) for Safe 
+    (2) for Super Safe 
+    >>> '''))
+checkout_type =int(input('''Enter:
+    (1) Free Checkout
+    (2) Paid Checkout
+    >>> '''))
+linkmode = int(input('''Enter:
+    (1) dashboard link
+    (2) all
+    (3) pass
+    >>>  '''))
